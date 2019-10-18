95f1c760b77d2d1f194769d2c407073cf74e71f8
Roberto Carnaúba Dias <betocarnauba@hotmail.com>
2019-10-18 16:58:35 -0300

    criação de express session
    Ciração de validação de usuario com session do express.

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,3 +1,13 @@
 module.exports.jogo = function(application, req, res){
-	res.render('jogo');
+	if(req.session.usuario){
+		res.render('jogo');
+	} else{
+		res.render('index', {validacao:{}})
+	}
+}
+
+module.exports.sair = function(application, req, res){
+	req.session.destroy( function(err){
+		res.render('index', {validacao:{}})
+	})
 }
--- mmorpg_got/mmorpg_got/app/routes/aldeoes.js
+++
@@ -1,5 +0,0 @@
-module.exports =  function(app){
-  app.get('/aldeoes', function(req, res){
-      res.render('aldeoes');
-  })
-}
